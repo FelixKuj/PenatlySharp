a5aba578ed8bf2b05fc1731308b0d933b3d7ce47
Felixkuj <felix.kujanpaa@gmail.com>
2014-12-08 11:22:15 +0100

    Utvecklat Spelare.cs

--- PenaltySharp/Model/Spelare.cs
+++ PenaltySharp/Model/Spelare.cs
@@ -8,7 +8,71 @@ namespace PenaltySharp.Model
 {
     class Spelare
     {
-        string name;
-        int id;
+        private string namn;
+        private int id;
+        private string användarnamn;
+        private string lösenord;
+
+        public Spelare()
+        {
+            this.namn = "Felix Kujanpää";
+            this.id = 0;
+            this.användarnamn = "FelixKuj";
+            this.lösenord = "QWE123";
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="inNamn">Spelare namn</param>
+        /// <param name="inId">Spelare ID</param>
+        /// <param name="inAnvändarnamn">Spelare användarnamn</param>
+        /// <param name="inLösenord">Spealre Lösenord</param>
+        public Spelare(string inNamn,
+                     int inId,
+                     string inAnvändarnamn,
+                     string inLösenord)
+        {
+            this.namn = inNamn;
+            this.id = inId;
+            this.användarnamn = inAnvändarnamn;
+            this.lösenord = inLösenord;
+        }
+
+        /// <summary>
+        /// rerurnerar Namn
+        /// </summary>
+        /// <returns>Namn</param>
+        public string getNamn(string Namn)
+        {
+           return this.namn;
+        }
+
+        /// <summary>
+        /// Bestämmer Namn 
+        /// </summary>
+        /// <param name="namn">Namn</param>
+        public void setNamn(string namn)
+        {
+            this.namn = namn;
+        }
+
+        /// <summary>
+        /// Returnerar ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>ID</returns>
+        public int getId(string id)
+        {
+            return this.id;
+        }
+        /// <summary>
+        /// Returnerar ID
+        /// </summary>
+        /// <param name="id">ID</param>
+        public setId(string id)
+        {
+            this.id = id;
+        }
     }
 }

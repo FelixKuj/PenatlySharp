0f31f3a40ba2e4006f96b471a016032b7cb8ee83
Vodkamartini <martin.marklund@hotmail.se>
2014-12-08 10:58:50 +0100

    Utvecklade get/set metoder för befintliga medlemsvariabler.

--- PenaltySharp/Model/Regler.cs
+++ PenaltySharp/Model/Regler.cs
@@ -12,11 +12,11 @@ namespace PenaltySharp.Model
         private string namn;
         private int id;
         private int böter;
-        //public string namn { get; set; }
+       
 
         public Regler()
         {
-            this.namn = "hångel";
+            this.namn = "Hångel";
             this.id = 0;
             this.böter = 450000000;
         }
@@ -27,5 +27,52 @@ namespace PenaltySharp.Model
             this.id = inId;
             this.böter = inBöter;
         }
+
+        // metoder
+
+        /// <summary>
+        ///Returnerar regel namn
+        /// </summary>
+        /// <returns>regel namn</returns>
+        public string getNamn()
+        {
+            return this.namn;
+        }
+
+
+        /// <summary>
+        /// Returnerar regel id.
+        /// </summary>
+        /// <returns>regel id</returns>
+        public int getId()
+        {
+            return this.id;
+        }
+
+        /// <summary>
+        /// Returnerar regel böter.
+        /// </summary>
+        /// <returns>regel böter</returns>
+        public int getBöter()
+        {
+            return this.böter;
+        }
+       
+        public void setNamn(string namn)
+        {
+            this.namn = namn;
+        }
+
+      
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public void setBöter(int böter)
+        {
+            this.böter = böter;
+        }
+
     }
 }

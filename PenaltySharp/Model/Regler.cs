e5e6e69d2ea580f2fa373609eb737c606ea85bf9
Vodkamartini <martin.marklund@hotmail.se>
2014-12-08 10:37:09 +0100

    Skapade en regel med lite olika parametrar. Gjorde this.grejs saken

--- PenaltySharp/Model/Regler.cs
+++ PenaltySharp/Model/Regler.cs
@@ -12,6 +12,20 @@ namespace PenaltySharp.Model
         private string namn;
         private int id;
         private int böter;
-       
+        //public string namn { get; set; }
+
+        public Regler()
+        {
+            this.namn = "hångel";
+            this.id = 0;
+            this.böter = 450000000;
+        }
+
+        public Regler(string inNamn, int inId, int inBöter)
+        {
+            this.namn = inNamn;
+            this.id = inId;
+            this.böter = inBöter;
+        }
     }
 }

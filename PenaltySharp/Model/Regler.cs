f00ed8d695f6ef88e6d0083be25f3be5fa8784ec
Vodkamartini <martin.marklund@hotmail.se>
2014-12-08 10:25:17 +0100

    Skapade class Regler med string namn, int id och int böter

---
+++ PenaltySharp/Model/Regler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenaltySharp.Model
+{
+    class Regler
+    {
+        //medlemsvariabler
+        private string namn;
+        private int id;
+        private int böter;
+       
+    }
+}

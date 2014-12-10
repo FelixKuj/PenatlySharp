22392f25b9bd3d151906f9d23b78c7e0826fe8b3
jallewalle <jallewalle@gmail.com>
2014-12-10 14:11:56 +0100

    Började Controler foldern och skrev lite kod

---
+++ PenaltySharp/Controler/Controler.cs
@@ -0,0 +1,19 @@
+﻿using PenaltySharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenaltySharp.Controler
+{
+    class Controler
+    {
+        private List<Spelare> m_Spelare;
+
+        public SpelarLista()
+        {
+            m_Spelare = new List<Spelare>();
+        }
+    }
+}

829de840758ed9e876aeda188d74503baf5c497f
jallewalle <jallewalle@gmail.com>
2014-12-10 14:24:23 +0100

    Spelare Controller har skapats, utveckats lite.

---
+++ PenaltySharp/Controller/SpelareController.cs
@@ -0,0 +1,39 @@
+﻿using PenaltySharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenaltySharp.Controler
+{
+    class SpelareController
+    {
+        private List<Spelare> m_Spelare;
+
+        public SpelareController()
+        {
+            m_Spelare = new List<Spelare>();
+        }
+
+        public void Läggatill(Spelare item)
+        {
+            m_Spelare.Add(item);
+        }
+        public void Tabort(Spelare item)
+        {
+            m_Spelare.Remove(item);
+        }
+
+        public Spelare Get(int index)
+        {
+            return m_Spelare.ElementAt(index);
+        }
+
+        public int Antal()
+        {
+            return m_Spelare.Count();
+        }
+    }
+}
+

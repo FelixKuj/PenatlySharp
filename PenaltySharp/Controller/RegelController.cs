73f2441728312a030029127718b67674672dd2ca
Vodkamartini <martin.marklund@hotmail.se>
2015-01-19 10:53:31 +0100

    Såg till att man kunde öppna nya fönster

--- PenaltySharp/Controller/RegelController.cs
+++ PenaltySharp/Controller/RegelController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PenaltySharp.Model;
+using PenaltySharp.Controller;
+using PenaltySharp.View;
 
 namespace PenaltySharp.Controller
 {
--- PenaltySharp/Program.cs
+++ PenaltySharp/Program.cs
@@ -20,6 +20,7 @@ namespace PenaltySharp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new View.Inloggning());
+
         }
     }
 }

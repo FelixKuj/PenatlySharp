7d5038f577803bba150f7b4f7ce036114c04257f
felixkuj <felix.kujanpaa@gmail.com>
2015-01-21 15:00:16 +0100

    ändratr lite

--- PenaltySharp/View/Register.cs
+++ PenaltySharp/View/Register.cs
@@ -7,18 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PenaltySharp.Model;
+using PenaltySharp.Controller;
+using PenaltySharp.View;
 
 namespace PenaltySharp.View
 {
     public partial class Register : Form
     {
+        SpelareController spelarController;
         public Register()
         {
             InitializeComponent();
+            spelarController = new SpelareController();
         }
 
         private void btn_RegistreringsSida_Registrera_Click(object sender, EventArgs e)
         {
+            
             this.Hide();
             
         }

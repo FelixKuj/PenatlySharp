ee749dbf1c970261789503f5bbaa682ce1c42877
Vodkamartini <martin.marklund@hotmail.se>
2015-02-04 14:20:33 +0100

    Fixade lite grejs

--- PenaltySharp/Controller/RegelController.cs
+++ PenaltySharp/Controller/RegelController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PenaltySharp.Model;
 using PenaltySharp.Controller;
 using PenaltySharp.View;
 using System.Windows.Forms;

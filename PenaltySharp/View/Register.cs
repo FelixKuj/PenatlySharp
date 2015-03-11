91c9cf30cca734d57b7833e640cc0600e1212669
felixkuj <felix.kujanpaa@gmail.com>
2015-03-11 14:46:17 +0100

    Mer Registrering

--- PenaltySharp/View/Register.cs
+++ PenaltySharp/View/Register.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using PenaltySharp.Model;
 using PenaltySharp.Controller;
 using PenaltySharp.View;
+using System.Net.Mail.MailAddress;
 
 namespace PenaltySharp.View
 {
@@ -22,6 +23,8 @@ namespace PenaltySharp.View
         bool FelFörnamn;
         bool FelLösenord;
         bool FelLösenordsjämförelse;
+        bool FelNågot;
+        string FelMedlenade;
 
         public Register()
         {
@@ -31,40 +34,74 @@ namespace PenaltySharp.View
 
         private void btn_RegistreringsSida_Registrera_Click(object sender, EventArgs e)
         {
-            if (tbx_RegistreringsSida_användarnamn.TextLength >= 6 &&
-    tbx_RegistreringsSida_Efternamn.Text != null &&
-    tbx_RegistreringsSida_Email.Text.Contains('@') &&
-    tbx_RegistreringsSida_Förnamn.Text != null &&
-    tbx_RegistreringsSida_lösenord.TextLength >= 6 &&
-    tbx_RegistreringsSida_LösenordIgen.Text == tbx_RegistreringsSida_lösenord.Text)
+            for (int i = 0; i < spelarController.Antal(); i++)
+            {
+                if (tbx_RegistreringsSida_användarnamn.Text == spelarController.GetAnvändarnamn(i))
+                {   FelMedlenade += "Användarnamnet existerar redan.\n";
+                    FelAnvändarnamn = true;
+                    FelNågot = true;
+                }
+            }
+            if (tbx_RegistreringsSida_användarnamn.TextLength <= 6 || tbx_RegistreringsSida_användarnamn.TextLength <= 18)
+            {
+                FelMedlenade += "Användarnamnet är för kort eller långt.\n";
+                FelAnvändarnamn = true;
+                FelNågot = true;
+            }
+            if (tbx_RegistreringsSida_Efternamn.TextLength < 6)
+            {
+                FelMedlenade += "Har du inget efternamn?\n";
+                FelEfternamn = true;
+                FelNågot = true;
+            }
+            //if (tbx_RegistreringsSida_Email.Text.Contains('@') == true)
+            //{
+            //    FelMedlenade += "Tror inte det där är en Email va.\n";
+            //    FelEmail = true;
+            //    FelNågot = true;
+            //}
+
+            if (tbx_RegistreringsSida_Förnamn.Text != null)
+            {
+                FelMedlenade += "Har du inget förnamn?\n";
+                FelFörnamn = true;
+                FelNågot = true;
+            }
+            if (tbx_RegistreringsSida_lösenord.TextLength >= 6 || tbx_RegistreringsSida_lösenord.TextLength <= 18)
+            {
+                FelMedlenade += "Lösenorden Är för kort eller långt.\n";
+                FelLösenord = true;
+                FelNågot = true;
+            }
+            if (tbx_RegistreringsSida_LösenordIgen.Text == tbx_RegistreringsSida_lösenord.Text)
+            {
+                FelMedlenade += "Lösenorden Matchar inte.\n";
+                FelLösenordsjämförelse = true;
+                FelNågot = true;
+            }
+            if (FelNågot == true)
+            {
+                MessageBox.Show(FelMedlenade);
+            }
+            else
             {
                 spelarController.SkapaAnvändare(tbx_RegistreringsSida_Förnamn.Text, tbx_RegistreringsSida_Efternamn.Text, tbx_RegistreringsSida_användarnamn.Text, tbx_RegistreringsSida_lösenord.Text, tbx_RegistreringsSida_LösenordIgen.Text, tbx_RegistreringsSida_Email.Text);
                 this.Hide();
             }
-            if (tbx_RegistreringsSida_användarnamn.TextLength < 6)
-            {
-                FelAnvändarnamn = true;
-            }
-            if (tbx_RegistreringsSida_Efternamn.TextLength < 6)
-            {
-                FelEfternamn = true;
-            }
-            if (tbx_RegistreringsSida_Email.Text.Contains('@'))
-            {
-                FelEmail = true;
-            }
-            if (tbx_RegistreringsSida_Förnamn.Text != null)
-            {
-                FelFörnamn = true;
-            }
-            if (tbx_RegistreringsSida_lösenord.TextLength >= 6)
-            {
-                FelLösenord = true;
-            }
-            if (tbx_RegistreringsSida_LösenordIgen.Text == tbx_RegistreringsSida_lösenord.Text)
-            {
-                FelLösenordsjämförelse = true;
-            }
+
         }
+        //public bool IsValid(string emailaddress)
+        //{
+        //    try
+        //    {
+        //        MailAddress m = new MailAddress(emailaddress);
+
+        //        return true;
+        //    }
+        //    catch (FormatException)
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 }

8c70a78446009b3a1ec950b610e2f22aeeae9980
jallewalle <jallewalle@gmail.com>
2015-03-25 15:10:41 +0100

    fixade view med alla spelare

---
+++ PenaltySharp/View/SpelareSida.cs
@@ -0,0 +1,46 @@
+﻿using PenaltySharp.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PenaltySharp.View
+{
+    public partial class SpelareSida : Form
+    {
+        SpelareController spelarecontroller;
+        public SpelareSida()
+        {
+            spelarecontroller = ServiceProvider.GetSpelareService();
+            InitializeComponent();
+        }
+        
+        private void SpelareSida_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                try
+                {
+                    for (int i = 0; i < spelarecontroller.Antal(); i++)
+                    {
+                        if (lv_SpelareSida.Items[i].Selected)
+                        {
+                            lv_SpelareSida.Items.RemoveAt(i);
+                            //regelcontroller.RemoveAt(i);
+
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}

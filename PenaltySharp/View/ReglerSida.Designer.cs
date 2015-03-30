8f9245b32d90b64efeb4e23d51358d8c7a145613
Vodkamartini <martin.marklund@hotmail.se>
2015-03-30 10:48:00 +0200

    Fortsatt kommentering av koden

--- PenaltySharp/View/ReglerSida.Designer.cs
+++ PenaltySharp/View/ReglerSida.Designer.cs
@@ -109,7 +109,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 447);
+            this.ClientSize = new System.Drawing.Size(381, 447);
             this.Controls.Add(this.lv_ReglerSida);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);

85d00d957237c18e2fd71e05cf770e017ffcaf3e
felixkuj <felix.kujanpaa@gmail.com>
2015-05-13 13:30:09 +0200

    fixade böter.dat

--- PenaltySharp/Controller/BöterController.cs
+++ PenaltySharp/Controller/BöterController.cs
@@ -26,7 +26,7 @@ namespace PenaltySharp.Controller
           public BöterController()
         {
             m_böter = new List<Böter>();
-            //testData();
+            
             try
             {
                 if (File.Exists("BöterLista.DAT"))
@@ -47,7 +47,7 @@ namespace PenaltySharp.Controller
         public void Add(Böter item)
         {
             m_böter.Add(item);
-           // ServiceProvider.GetBöterService().BinarySerialize();
+            ServiceProvider.GetBöterService().BinarySerialize();
         }
 
         /// <summary>

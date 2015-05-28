12f5a69a507e20fccf0b1b97664c71c532202da0
jallewalle <jallewalle@gmail.com>
2015-05-28 10:49:58 +0200

    fixade diagram view model vcontroller

--- PenaltySharp/View/DiagramView.Designer.cs
+++ PenaltySharp/View/DiagramView.Designer.cs
@@ -36,19 +36,19 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(140, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Diagram 1";
+            this.button1.Text = "Antal böter per spelare";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(487, 12);
+            this.button2.Location = new System.Drawing.Point(452, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(110, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Diagram 2";
+            this.button2.Text = "Antal regler brutna";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
